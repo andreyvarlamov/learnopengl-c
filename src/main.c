ab5861ed95e4724156eb6cfa1cf88468eaadb4cd
Andrey Varlamov <varlamov.and@gmail.com>
2023-02-07 21:03:24 -0800

    Create GLFW and load GLAD

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        printf("Error when creating a GLFW window.\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        printf("Error when loading GLAD.\n");
+        return -1;
+    }
+
+    while(1) 
+    {
+    }
+
+    return 0;
+}
