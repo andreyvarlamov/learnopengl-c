3c0bd94fa09bd2a3bd8e17bb5433fccca7666ea5
Andrey Varlamov <varlamov.and@gmail.com>
2023-02-08 17:23:43 -0800

    Fragment shader

--- src/main.c
+++ src/main.c
@@ -4,13 +4,22 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-// Define shader source code
+// Define vertex shader source code
 const char *vertexShaderSource =
     "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
     "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+// Define fragment shader source code
+const char *fragmentShaderSource =
+    "#version 330 core\n"
+    "out vec4 FragColor;"
+    "void main()\n"
+    "{\n"
+    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
     "}\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -68,6 +77,22 @@ int main()
     {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
         printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n");
+        printf(infoLog, '\n');
+    }
+
+    // Compile the fragment shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Check if the compilation was successful
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    if (!fragmentSuccess)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n");
         printf(infoLog, '\n');
     }
 
